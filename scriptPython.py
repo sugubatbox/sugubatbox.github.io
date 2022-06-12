f55067b218a8ae36e1dc68957b5ee7f1393f2628
sugubatbox <107370791+sugubatbox@users.noreply.github.com>
2022-06-13 00:08:00 +0300

    Add files via upload

---
+++ scriptPython.py
@@ -0,0 +1,31 @@
+
+# import cgi,os
+import cgitb; cgitb.enable()
+
+
+print("Content-Type: text/html")  
+print("<TITLE>CGI script output</TITLE>")
+print("<H1>This is my first CGI script</H1>")
+print("Hello, world!")
+# # form=cgi.MiniFieldStorage()
+# # email=str(form.name())
+# # form=cgi.FieldStorage()
+# # email=str(form.getvalue('email'))
+
+# # print(email)
+
+# print('<html>')
+# print('<head>')
+# print('<title>buahahah</title>')
+# print('</head>')
+# print('<body>')
+# print('<h1>asta e baa</h1>')
+# print('</body>')
+# print('</html>')
+
+
+
+# print('<body><center>')
+# print('<h1>Email: </h1>')
+# print('<center><body><html>')
+# print("hello")
